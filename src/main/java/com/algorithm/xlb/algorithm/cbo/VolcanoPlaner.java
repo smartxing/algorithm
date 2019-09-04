d15ed8490aae48265b8c85f1a2798b757d598fa6
xingliangbo <xingliangbo@u51.com>
2019-09-04 10:12:47 +0800

    init

---
+++ src/main/java/com/algorithm/xlb/algorithm/cbo/VolcanoPlaner.java
@@ -0,0 +1,10 @@
+package com.algorithm.xlb.algorithm.cbo;
+
+/**
+ *
+ * cbo 代价优化算法
+ * @author xingliangbo
+ * @version $Id: v 0.1 2019-09-04 10:12 xingliangbo Exp $
+ */
+public class VolcanoPlaner {
+}
