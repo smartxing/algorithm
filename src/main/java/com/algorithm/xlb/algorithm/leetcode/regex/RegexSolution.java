8ab70ac34120721b851f8d57c5b489fbb2a14329
xingliangbo <xingliangbo@u51.com>
2019-09-09 10:09:32 +0800

    regex

--- src/main/java/com/algorithm/xlb/algorithm/leetcode/regex/RegexSolution.java
+++ src/main/java/com/algorithm/xlb/algorithm/leetcode/regex/RegexSolution.java
@@ -1,6 +1,7 @@
 package com.algorithm.xlb.algorithm.leetcode.regex;
 
 /**
+ * isMatch 递归 isMatch2 动态规划
  * https://leetcode-cn.com/problems/regular-expression-matching/
  * @author xingliangbo
  * @version $Id: v 0.1 2019-09-06 11:51 xingliangbo Exp $
@@ -32,11 +33,58 @@ public class RegexSolution {
     }
   }
 
+
+  /**
+   *
+   *    eg :   aaa   匹配 a*
+   *      __________________
+   *      |   | "" | a | a  |
+   *      |"" | __ | __| __ |
+   *      | a | __ | __| __ |
+   *      | * | __ | __| __ |
+   *
+   *
+   * 如果s1的第 i 个字符和s2的第 j 个字符相同，或者s2的第 j 个字符为 “.”
+   *f[i][j] = f[i - 1][j - 1]
+   *  如果s2的第 j 个字符为 *
+   * 若s2的第 j 个字符匹配 0 次第 j - 1 个字符
+   * f[i][j] = f[i][j - 2] 比如(ab, abc*)
+   * 若s2的第 j 个字符匹配至少 1 次第 j - 1 个字符,
+   * f[i][j] = f[i - 1][j] and s1[i] == s2[j - 1] or s[j - 1] == '.'
+   * 这里注意不是 f[i - 1][j - 1]， 举个例子就明白了 (abbb, ab*) f[4][3] = f[3][3]
+   */
+  public static boolean isMatch2(String s, String p) {
+    int m = s.length(), n = p.length();
+    boolean[][] f = new boolean[m + 1][n + 1];
+
+    f[0][0] = true;
+    for (int i = 2; i <= n; i++) {
+      //如果匹配0个*
+      f[0][i] = f[0][i - 2] && p.charAt(i - 1) == '*';
+    }
+    for (int i = 1; i <= m; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+          f[i][j] = f[i - 1][j - 1];
+        }
+        if (p.charAt(j - 1) == '*') {
+          f[i][j] = f[i][j - 2] ||
+              f[i - 1][j] && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.');
+        }
+      }
+    }
+
+    return f[m][n];
+
+
+  }
+
+
   public static void main(String[] args) {
-    System.out.println(isMatch("aaa", "a*"));
-    System.out.println(isMatch("aaaB", "a*B"));
-    System.out.println(isMatch("abcd", ".*B"));
-    System.out.println(isMatch("abcdB", ".*B"));
+    System.out.println(isMatch2("aaa", "a*"));
+    System.out.println(isMatch2("aaaB", "a*B"));
+    System.out.println(isMatch2("abcd", ".*B"));
+    System.out.println(isMatch2("abcdB", ".*B"));
   }
 
 }
